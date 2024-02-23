ac5a0d7cfbf2ded6739df3e4d9c22c9094c744d9
Vadim Lemmer <lemmeronne@gmail.com>
2024-02-23 14:29:04 +0600

    feat: добавлена функция квадрата суммы двух чисел

---
+++ Helpers.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int sum(int a, int b)
+{
+	return std::pow(a + b, 2);
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "Helpers.h"
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a, b;
+
+	std::cout << "Число два числа: ";
+	std::cin >> a >> b;
+	std::cout << "Квадрат суммы двух чисел: " << sum(a, b);
+}
